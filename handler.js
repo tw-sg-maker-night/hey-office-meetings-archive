e5120de2929a78c66444eb30d5906ec0491e2fb8
Colin Harris <col.w.harris@gmail.com>
2016-12-25 20:35:38 +0800

    initial commit

---
+++ handler.js
@@ -0,0 +1,107 @@
+'use strict';
+
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var moment = require('moment');
+var Promise = require('promise');
+
+function authorize() {
+  return new Promise(function (fulfill, reject) {
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(
+      process.env['GOOGLE_CLIENT_ID'],
+      process.env['GOOGLE_CLIENT_SECRET'],
+      process.env['GOOGLE_REDIRECT_URL']
+    );
+    oauth2Client.credentials = {
+      access_token: process.env['GOOGLE_ACCESS_TOKEN'],
+      refresh_token: process.env['GOOGLE_REFRESH_TOKEN'],
+      token_type: process.env['GOOGLE_TOKEN_TYPE'],
+      expiry_date: process.env['GOOGLE_EXPIRY_DATE']
+    };
+    fulfill(oauth2Client);
+  });
+}
+
+function createEvent(auth, title, room, start, end) {
+  return new Promise(function (fulfill, reject) {
+    var calendar = google.calendar('v3');
+    calendar.events.insert({
+      auth: auth,
+      calendarId: "primary",
+      resource: {
+          start: {
+            dateTime: moment(start).format(),
+            timeZone: "Asia/Singapore"
+          },
+          end: {
+            dateTime: moment(end).format(),
+            timeZone: "Asia/Singapore"
+          },
+          summary: title,
+          description: "Booked via Hey Office!",
+          attendees: [
+            {email: "thoughtworks.com_3439353432373934323230@resource.calendar.google.com"}
+          ]
+      }},
+      function (err, calendarEvent) {
+        if (err) {
+          reject(err);
+        } else {
+          fulfill(calendarEvent);
+        }
+      }
+    );
+  });
+}
+
+function validateData(data) {
+    if (!data.title) {
+      return "Missing required param: title";
+    }
+
+    if (!data.room) {
+      return "Missing required param: room";
+    }
+
+    if (!data.start) {
+      return "Missing required param: start";
+    }
+
+    if (!data.end) {
+      return "Missing required param: end";
+    }
+
+    return false;
+}
+
+module.exports.create = (event, context, callback) => {
+  console.log("Request received:\n", JSON.stringify(event));
+  console.log("Context received:\n", JSON.stringify(context));
+
+  const data = JSON.parse(event.body);
+
+  var validationError = validateData(data);
+  if (validationError) {
+    callback(null, {statusCode: 400, body: JSON.stringify({message: validationError})});
+    return;
+  }
+
+  authorize().then(function(auth) {
+    return createEvent(auth, data.title, data.room, data.start, data.end)
+  }).then(function(calendarEvent) {
+    console.log("Successfully created a calendar event: " + calendarEvent);
+    callback(null, {
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify({message: "Booked "+data.room+" successfully"})
+    });
+  }).catch(function(err) {
+    console.log("Failed to create a calendar event: " + err);
+    callback(err, {
+        statusCode: 400,
+        headers: {},
+        body: JSON.stringify({message: ""+err})
+    });
+  }).done();
+};
